d5aa8f61db148d64eefac448adf4ff34656b4a65
Nicholas Neo <nicholasneo78@gmail.com>
2020-10-22 09:23:59 +0800

    Added files for Assignment 1

---
+++ MH3510 Assignment 1/Neo_Shun_Xian_Nicholas_Assignment_1[OLD].R
@@ -0,0 +1,24 @@
+# Store the data into variable X & Y
+X <- c(40,50,60,70,80,90,40,60,80,50)
+Y <- c(69,175,272,335,490,415,72,265,492,180)
+
+# a) Fit simple regression to the data
+slr <- lm(Y~X)
+# Display the information of this model
+slr
+# Plot the data
+plot(X,Y)
+# Fit a simple linear regression line to the plot
+abline(slr)
+
+# b) Obtain the residuals and examine them
+# Obtain residuals
+residual <- slr$res
+# display the residuals value
+residual
+# examine the residuals by plotting the residual against X plot
+plot(X,residual)
+
+summary(slr)
+
+anova(slr)
---
+++ MH3510 Assignment 1/README.txt
@@ -0,0 +1,13 @@
+Files Submitted
+	- Neo_Shun_Xian_Nicholas_Assignment_1.pdf
+	- Neo_Shun_Xian_Nicholas_Assignment_1.Rmd
+	- raw_code_assignment_1.R
+
+Neo_Shun_Xian_Nicholas_Assignment_1.pdf
+- A formal report of Assignment 1 that is to be submitted for grading
+
+Neo_Shun_Xian_Nicholas_Assignment_1.Rmd
+- A markdown file that is used to generate Neo_Shun_Xian_Nicholas_Assignment_1.pdf
+
+raw_code_assignment_1.R
+- A file that only contains raw code for the assignment (no explanation)
