08083cd378eab8bd04585ddce277653927f46fab
Nicholas Neo <nicholasneo78@gmail.com>
2020-10-22 09:47:09 +0800

    updated project folder

--- MH3510 Project/TRYOUT_steam_data_analysis.R
+++ MH3510 Project/TRYOUT_steam_data_analysis.R
@@ -1,2 +1,25 @@
 # graphical display of the observed data
-steam_raw <- read.table
+steam_raw <- read.table("Steam.datt", header=FALSE)
+steam <- data.frame(y=steam_raw$V2, x1=steam_raw$V3, x2=steam_raw$V4, x3=steam_raw$V9)
+steam
+plot(steam)
+
+## modelling multiple linear regression
+# fit a MLR model
+mlr <- lm(y ~ x1+x2+x3, data=steam)
+summary(mlr)
+
+# extract regression quantities we need from the model object
+names(mlr)
+
+mlrs <- summary(mlr)
+names(mlrs)
+
+## adequacy checking (same case as SLR)
+# 4.1 from viewpoint of fitted model
+
+# 4.2 from viewpoint of residuals
+# normality checking
+qqnorm(residuals(mlr), ylab="residuals")
+qqline(residuals(mlr))
+
